4dec894d238875e0a1d210d81f8f62e421eb67a6
AlanPan0131 <panalan0131@gmail.com>
2022-10-15 15:14:27 +0800

    Update Client.py

--- code/Client.py
+++ code/Client.py
@@ -20,7 +20,16 @@ class ClientThread(threading.Thread):
 
     def run(self):
         client_msg = self.cSocket.recv(BUF_SIZE)
-        print(client_msg.decode('utf-8'))
+        while client_msg:
+            data  = client_msg.decode('utf-8')
+            while data[len(data) - 1] != '\0':
+                client_msg = self.cSocket.recv(BUF_SIZE)
+                data += client_msg.decode('utf-8')
+            
+            data = data.split('^^')
+            print(data[2])
+            print(f'from: {data[1]}\n------------')
+            client_msg = self.cSocket.recv(BUF_SIZE)
 
 def main():
 	# Get server IP
@@ -34,20 +43,21 @@ def main():
     cSocket.connect((serverIP, PORT))
     ClientThread(cSocket)
     # Send message to server
-    while(1):
-        try:
-            msg = str(input("message:"))
-            cmd = f"message,1,{sys.getsizeof(msg)},{msg}\0"
+    try:
+        while(1):
+            msg = str(input(""))
+            print('from: me\n------------')
+            cmd = f"message^^%id%^^{msg}\0"
+            #sys.getsizeof(msg)
             cSocket.send(cmd.encode('utf-8'))
-            
-        except socket.error as e:
-            print('Socket error: %s' % str(e))
-        except Exception as e:
-            print('Other exception: %s' % str(e))
-        finally:
-            print('Closing connection.')
+    except socket.error as e:
+        print('Socket error: %s' % str(e))
+    except Exception as e:
+        print('Other exception: %s' % str(e))
+    finally:
+        print('Closing connection.')
     		# Close the TCP socket
-    		#cSocket.close()
+    #cSocket.close()
 
 # end of main
 
