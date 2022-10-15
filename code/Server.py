ca0825fec2b3ca64be08216d6217f73dd21fb165
AlanPan0131 <panalan0131@gmail.com>
2022-10-15 13:56:21 +0800

    Update Server.py

--- code/Server.py
+++ code/Server.py
@@ -5,42 +5,63 @@
 #  2021.07.13                                                   									
 ####################################################
 import socket
+import threading
 
-PORT = 6001
+PORT = 6666
 backlog = 5
 BUF_SIZE = 1024			# Receive buffer size
 
+class ServerThread(threading.Thread):
+    def __init__(self, t_name, client_sc, rip, rport):
+        super().__init__(name = t_name)
+        self.client = client_sc
+        self.rip = rip
+        self.rport = rport
+        print(f"user connect {str(rip)}:{str(rport)}")
+        self.start()
+
+    def run(self):
+        #name = threading.current_thread().name
+        client_msg = self.client.recv(BUF_SIZE)
+
+        while client_msg:
+            cmd = client_msg.decode('utf-8').split(',')
+            self.client.send('111'.encode('utf-8'))
+            sizeOfMsg = int(cmd[2])
+            print(f'size:{sizeOfMsg}')
+            self.recvMsgContent(sizeOfMsg)
+            client_msg = self.client.recv(BUF_SIZE)
+        self.client.close()
+        
+    def recvMsgContent(self, size):
+        client_msg = self.client.recv(size)
+        print(client_msg.decode('utf-8'))
+
 def main():
 	# Create a TCP Server socket
-	srvSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srvSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	# Enable reuse address/port
-	srvSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srvSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	
 	# Bind 	on any incoming interface with PORT, '' is any interface
-	print('Starting up server on port: %s' % (PORT))
-	srvSocket.bind(('', PORT))
+    print('Starting up server on port: %s' % (PORT))
+    srvSocket.bind(('', PORT))
 	
 	# Listen incomming connection, connection number = backlog (5)
-	srvSocket.listen(backlog)
+    srvSocket.listen(backlog)
 	
 	# Accept the incomming connection
-	print('Waiting to receive message from client')
-	client, (rip, rport) = srvSocket.accept()
-	
-	# Receive client message, buffer size = BUF_SIZE
-	client_cmd = client.recv(BUF_SIZE)
-	if client_cmd:
-		cmd = client_cmd.decode('utf-8').split(',')
-		# msg = "from IP: " + str(rip) + " port: " + str(rport)
-		client.send('111'.encode('utf-8'))
-		sizeOfMsg = int(cmd[2])
-		print(f'size:{sizeOfMsg}')
-		client_msg = client.recv(sizeOfMsg)
-		if client_msg:
-			print(client_msg.decode('utf-8'))
-	client.close()
-	srvSocket.close()
+    counter = 1
+    while(1):
+        t_name = 'Thread ' + str(counter)
+        counter += 1
+        print('Waiting to receive message from client')
+        client, (rip, rport) = srvSocket.accept()
+        print('Got connection. Create thread: %s' % t_name)
+        ServerThread(t_name, client, rip, rport)
+    
+    srvSocket.close()
 # end of main
 
 if __name__ == '__main__':
