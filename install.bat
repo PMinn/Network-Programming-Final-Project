f624dab4616cb553348542a5c33f4e2f4392787b
AlanPan0131 <panalan0131@gmail.com>
2022-10-27 17:21:56 +0800

    update

--- install.bat
+++ install.bat
@@ -1,3 +1,20 @@
-pip install eel
-pip install pyautogui
+@ECHO OFF
+setlocal enabledelayedexpansion
+
+SET [ipList='']
+
+@REM pip install eel
+@REM pip install pyautogui
+
+cd ./module
+@REM  tokens=5
+for /f "skip=7 delims=" %%i in ('dir') do (
+	
+    for %%a in (%%i) do set ipList=!ipList!%%a$
+    set ipList=!ipList!#
+    @REM pip install ./
+)
+
+echo %ipList%
+  
 pause
