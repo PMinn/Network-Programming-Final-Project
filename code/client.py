6da1c0a74be4d8993b19cd6e8acfb6ec175ac12f
AlanPan0131 <panalan0131@gmail.com>
2022-12-10 16:57:01 +0800

    update
    fixed offline and disconnect

--- code/client.py
+++ code/client.py
@@ -13,6 +13,7 @@ BUF_SIZE = 1024
 serverIP = "127.0.0.1"
 reServerAddress = None
 imageThread = None
+page = "index"
 
 eel.init('web', allowed_extensions=['.js', '.html'])
 
@@ -34,6 +35,12 @@ except Exception as msg:
 @eel.expose
 def gethostname():
     return socket.gethostname()
+
+@eel.expose
+def changePage(targetPage):
+    global page
+    page = targetPage
+    return targetPage
 
 ''' supporter '''
 isSending = False
@@ -71,31 +78,34 @@ def sending(address):
     imgId = int(0)
     while isSending:
         splitedData = dataSplit(getScreenshotToBase64()+'@')
-        # data = getScreenshotToBase64()+'@'
         for data in splitedData:
             UDPSocket.sendto(('{:06d}'.format(imgId) + data).encode('utf-8'), address)
             time.sleep(0.04166667/len(splitedData))
             # 24fps == 0.04166667
         imgId += 1
 
-@eel.expose
-def signSupporter(hostname):
+def supporterMainThread():
     global isSending
-    print("signSupporter")
-    TCPSocket.send(f"signSupporter,{hostname}".encode('utf-8'))
-    threading.Thread(target=sendCheckTime).start()
     while 1:
         msg = TCPSocket.recv(BUF_SIZE)
         data = msg.decode('utf-8').split(',')
-        if data[0] == 'connect2Supporter':
+        if data[0] == 'connect':
             isSending = True
             address = data[1].split(':')
             address = (address[0],int(address[1]))
             threading.Thread(target=checkControl).start()
             threading.Thread(target=sending, args=(address,)).start()
-        elif data[0] == "disconnection":
+        elif data[0] == "disconnect":
             isSending = False
-            print("disconnection")
+            print("Supporter get disconnect")
+
+@eel.expose
+def signSupporter(hostname):
+    print("signSupporter")
+    TCPSocket.send(f"signSupporter,{hostname}".encode('utf-8'))
+    # threading.Thread(target=sendCheckTime).start()
+    threading.Thread(target=supporterMainThread).start()
+    
 ''' end supporter '''
 
 ''' access '''
@@ -137,7 +147,7 @@ def connect2Supporter(uid):
     global BUF_SIZE, reServerAddress, imageThread, targetUid, isRecving
     isRecving = True
     targetUid = uid
-    TCPSocket.send(f"connect2Supporter,{uid}".encode('utf-8'))
+    TCPSocket.send(f"connect,{uid}".encode('utf-8'))
     msg = TCPSocket.recv(BUF_SIZE)
     addr = msg.decode('utf-8').split(':')
     reServerAddress = (addr[0],int(addr[1]))
@@ -154,16 +164,31 @@ def mousemove(x,y):
 def close_callback(strPath, sockets):
     global BUF_SIZE, reServerAddress, imageThread, targetUid, isRecving, isSending
     print(f"close:{strPath},{sockets}")
-    if strPath == "show.html": # access close
+    if strPath == "show.html": # connected access close
         isRecving = False
-        TCPSocket.send(f"disconnection,{targetUid}".encode('utf-8'))
+        TCPSocket.send(f"disconnect,{targetUid}".encode('utf-8'))
         TCPSocket.close()
         sys.exit()
     elif strPath == "support.html": # support close
-        isSending = False
-        TCPSocket.send(f"unsupport,{targetUid}".encode('utf-8'))
-        TCPSocket.close()
-        sys.exit()
+        if isSending:
+            isSending = False
+            TCPSocket.send(f"disconnect,{targetUid}".encode('utf-8'))
+            TCPSocket.close()
+            sys.exit()
+        else:
+            TCPSocket.send("offline".encode('utf-8'))
+            TCPSocket.close()
+            sys.exit()
+    elif strPath == "access.html": # access close
+        if page == "access":
+            TCPSocket.send(f"offline".encode('utf-8'))
+            TCPSocket.close()
+            sys.exit()
+    elif strPath == "index.html": # access close
+        if page == "index":
+            TCPSocket.send(f"offline".encode('utf-8'))
+            TCPSocket.close()
+            sys.exit()
 
 eel.start('index.html', size=(1000, 1000), port=0, close_callback=close_callback)  # Start
 TCPSocket.close()
--- code/server.py
+++ code/server.py
@@ -39,41 +39,38 @@ def mainThread(clientSocket, rAddress):
         if data[0] == 'signSupporter':
             device = Supporter(device, data[1])
             supporters.append(device)
-            print(device)
+            print('supporter',device.uid,data[1])
         elif data[0] == 'checkTime':
             device.checkTime()
         elif data[0] == 'getSupporter':
             clientSocket.send(str(supporters).encode('utf-8'))
-        elif data[0] == 'connect2Supporter':
+        elif data[0] == 'connect':
             targetSupporter = supporters.find(data[1])
             clientSocket.send(f"{targetSupporter.UDPaddress[0]}:{targetSupporter.UDPaddress[1]}".encode('utf-8'))
-            targetSupporter.TCPsocket.send(f'connect2Supporter,{device.UDPaddress[0]}:{device.UDPaddress[1]}'.encode('utf-8'))
+            targetSupporter.TCPsocket.send(f'connect,{device.UDPaddress[0]}:{device.UDPaddress[1]}'.encode('utf-8'))
             targetSupporter.isRuning = True
-        elif data[0] == 'disconnection':
+        elif data[0] == 'disconnect':
             targetSupporter = supporters.find(data[1])
-            targetSupporter.TCPsocket.send('disconnection'.encode('utf-8'))
+            targetSupporter.TCPsocket.send('disconnect'.encode('utf-8'))
             targetSupporter.isRuning = False
-        elif data[0] == 'unsupport':
-            targetSupporter = supporters.find(data[1])
-            targetSupporter.TCPsocket.send('disconnection'.encode('utf-8'))
-            targetSupporter.isRuning = False
+        elif data[0] == 'offline':
+            supporters.removeDevice(device)
+            devices.removeDevice(device)
+            print("offline",device.uid)
+            clientSocket.close()
+            break
 
         try:
             client_msg = clientSocket.recv(BUF_SIZE)
         except:
-            supporters.removeDevice(device)
-            devices.removeDevice(device)
-            print(f"device ({device.uid}) offline")
-            clientSocket.close()
             break
-
+        
 def UDPThread():
     while 1:
         try:
             UDPSocket.settimeout(5)
             data, address = UDPSocket.recvfrom(BUF_SIZE)
             targetUID = data.decode('utf-8')
-            print(targetUID)
             targetDevices = devices.find(targetUID)
             if targetDevices != None:
                 targetDevices.setUDP(address)
@@ -83,7 +80,6 @@ def UDPThread():
 try:
     while True:
         clientSocket, rAddress = TCPSocket.accept()
-        print(str(rAddress[0])+" : "+str(rAddress[1]))
         try:
             threading.Thread(target=mainThread,args=(clientSocket, rAddress)).start()
             threading.Thread(target=UDPThread).start()
